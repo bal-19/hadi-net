241870da4ca85798dd88ad278d079b8d11cc4ebb
bal-19 <iqbalhaidee19@gmail.com>
2025-01-22 11:42:57 +0700

    upd: updating role column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
+            $table->enum('role', ['Super Admin', 'Admin', 'Technician', 'User']);
             $table->string('name');
             $table->enum("gender", ["male", "female", "other"]);
             $table->integer('age');
