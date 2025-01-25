7b90446c0bb976f57857a0676f6db6fe23930f5f
bal-19 <iqbalhaidee19@gmail.com>
2025-01-25 15:18:37 +0700

    feat: add function to show & insert user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,41 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    // function to show all user
+    public function index()
+    {
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
 
+    // function to show detail user
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        return response()->json($user);
+    }
+
+    // function to store user data
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'role' => 'required|in:Super Admin,Admin,Technician,User',
+            'name' => 'required|string|max:255',
+            'gender' => 'required|in:male,female,other',
+            'age' => 'required|int|min:0',
+            'address' => 'required',
+            'phone_number' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+        User::create($validated);
+
+        return redirect()->route('users.index')->with('success', 'User created successfully');
+    }
 }
