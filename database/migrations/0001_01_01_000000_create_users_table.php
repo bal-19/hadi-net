252a1c4e20773285213756055377c42eb23d1114
bal-19 <iqbalhaidee19@gmail.com>
2025-01-25 15:19:49 +0700

    chore: updating phone_number column to unique

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->enum('role', ['Super Admin', 'Admin', 'Technician', 'User']);
+            $table->enum('role', ['Super Admin', 'Admin', 'Technician', 'User'])->default('User');
             $table->string('name');
             $table->enum("gender", ["male", "female", "other"]);
             $table->integer('age');
             $table->text('address')->nullable();
-            $table->string('phone_number');
+            $table->string('phone_number')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
