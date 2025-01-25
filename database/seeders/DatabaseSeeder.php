6729b835780b50c7a4ded6a6e07b9a31c3a33549
bal-19 <iqbalhaidee19@gmail.com>
2025-01-25 15:20:07 +0700

    feat: add some user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table("users")->insert([
+            [
+                "role" => "Super Admin",
+                "name" => "Vivi Maria",
+                "gender" => "female",
+                "age" => 25,
+                "address" => "Junggo Street",
+                "phone_number" => "08253991552",
+                "email" => "vivi.mar@gmail.com",
+                "password" => Hash::make("vvMaria123"),
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "role" => "Admin",
+                "name" => "Tono Sigura",
+                "gender" => "male",
+                "age" => 30,
+                "address" => "Pesanggrahan, Kota Batu",
+                "phone_number" => "08678312332",
+                "email" => "sigura.tn@gmail.com",
+                "password" => Hash::make("ton123"),
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "role" => "Technician",
+                "name" => "Edi Satria",
+                "gender" => "male",
+                "age" => 48,
+                "address" => "Junggo Street",
+                "phone_number" => "081531331552",
+                "email" => "edi.junggo@gmail.com",
+                "password" => Hash::make("ediJunior"),
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+            [
+                "role" => "User",
+                "name" => "Harunkun Etyav",
+                "gender" => "other",
+                "age" => 18,
+                "address" => "Beji",
+                "phone_number" => "081556715532",
+                "email" => "run.kun@gmail.com",
+                "password" => Hash::make("kezet123"),
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,19 +12,5 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        DB::table("users")->insert([
-            "role" => "Super Admin",
-            "name" => "Vivi Maria",
-            "gender" => "female",
-            "age" => 25,
-            "address" => "Junggo Street",
-            "phone_number" => "08253991552",
-            "email" => "vivi.mar@gmail.com",
-            "password" => Hash::make("vvMaria123"),
-            "created_at" => now(),
-            "updated_at" => now()
-        ]);
-    }
+    public function run(): void {}
 }
