329c8b707b03d6429f7707afa08e424eb1f66e5d
bal-19 <iqbalhaidee19@gmail.com>
2025-01-30 15:02:29 +0700

    feat: add package & order routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\PackageController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home.index');
 });
-Route::get('/order', function () {
-    return view('order.index');
-});
 
 // Admin Route
 Route::prefix('admin')->group(function () {
     // Manage Users Route
     Route::resource('users', UserController::class);
+    // Manage Packages Route
+    Route::resource('packages', PackageController::class);
+    // Manage Orders Route
+    Route::resource('orders', OrderController::class);
 });
