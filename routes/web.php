f9065d97886c4f08e69e362a1b712cbd0601ab95
bal-19 <iqbalhaidee19@gmail.com>
2025-02-05 15:09:18 +0700

    feat: add middleware

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,14 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\PackageController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('home.index');
 // });
 
-Route::get('/order', [PackageController::class, 'listForUser'])->name('user.orders.create');
+Route::get('/order', [PackageController::class, 'listForUser'])->name('user.orders.create')->middleware('auth');
 
 // Admin Route
 Route::prefix('admin')->group(function () {
@@ -20,7 +21,7 @@ Route::prefix('admin')->group(function () {
     Route::resource('packages', PackageController::class);
     // Manage Orders Route
     Route::resource('orders', OrderController::class);
-});
+})->middleware('auth');
 
 // Auth Route
 Route::prefix('auth')->group(function () {
