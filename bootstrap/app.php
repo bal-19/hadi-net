7338bea10af192c944b9580bab8ee057b53f7a9e
bal-19 <iqbalhaidee19@gmail.com>
2025-02-05 19:03:57 +0700

    feat: add role.session middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'role.session' => \App\Http\Middleware\RoleSession::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
