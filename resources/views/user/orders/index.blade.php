ac70dde1436d322386f5206764db11f4e71795b1
bal-19 <iqbalhaidee19@gmail.com>
2025-02-06 14:04:38 +0700

    style: updating order form style

--- resources/views/user/orders/index.blade.php
+++ resources/views/user/orders/index.blade.php
@@ -4,33 +4,8 @@
 
 @section('content')
     <section class="py-8 antialiased md:py-16">
-        <form action="#" class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <form action="{{ route('user.order.store') }}" method="POST" class="mx-auto max-w-screen-xl px-4 2xl:px-0">
             @csrf
-            <ol
-                class="items-center flex w-full max-w-2xl text-center text-sm font-medium text-gray-500 dark:text-gray-400 sm:text-base">
-                <li
-                    class="after:border-1 flex items-center text-primary-700 after:mx-6 after:hidden after:h-1 after:w-full after:border-b after:border-gray-200 dark:text-primary-500 dark:after:border-gray-700 sm:after:inline-block sm:after:content-[''] md:w-full xl:after:mx-10">
-                    <span
-                        class="flex items-center after:mx-2 after:text-gray-200 after:content-['/'] dark:after:text-gray-500 sm:after:hidden">
-                        <svg class="me-2 h-4 w-4 sm:h-5 sm:w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            width="24" height="24" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-                        Checkout
-                    </span>
-                </li>
-
-                <li class="flex shrink-0 items-center">
-                    <svg class="me-2 h-4 w-4 sm:h-5 sm:w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    Order summary
-                </li>
-            </ol>
-
             <div class="mt-6 sm:mt-8 lg:flex lg:items-start lg:gap-12 xl:gap-16">
                 <div class="min-w-0 flex-1 space-y-8">
                     <div class="space-y-4">
@@ -48,6 +23,9 @@
 
                     <div class="space-y-4">
                         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">Package</h3>
+                        @error('package')
+                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                        @enderror
                         <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
                             @forelse ($packages as $package)
                                 <div
@@ -111,8 +89,8 @@
 
                     <div class="space-y-3">
                         <button type="submit"
-                            class="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Proceed
-                            to Payment</button>
+                            class="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Place
+                            an Order</button>
                     </div>
                 </div>
             </div>
