24058a6b1378327845d590ecbbe01ad3f36528f5
bal-19 <iqbalhaidee19@gmail.com>
2025-02-07 14:37:35 +0700

    feat: add overview at dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(Request $request)
+    {
+        $currentMonth = Carbon::now()->month;
+        $currentYear = Carbon::now()->year;
+
+        $totalUser = User::count();
+        $totalOrder = Order::count();
+        $totalRevenue = Order::sum('total');
+
+        // total order has paid status;
+        $paidOrders = Order::where('order_status', 'paid')
+            ->filter($request->only('search'))
+            ->orderBy('order_date', 'desc')
+            ->paginate(10)
+            ->withQueryString();
+
+        $dashboard = [
+            'totalUser' => $totalUser,
+            'totalOrder' => $totalOrder,
+            'totalRevenue' => $totalRevenue,
+            'paidOrders' => $paidOrders
+        ];
+
+        return view('admin.dashboard', compact('dashboard'));
+    }
+}
