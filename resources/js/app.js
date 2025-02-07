28589e3d552efb6f396b4b423277f9f0b7a05d78
bal-19 <iqbalhaidee19@gmail.com>
2025-02-07 14:37:04 +0700

    feat: add chart.js dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
-import './bootstrap';
-import 'flowbite';
-import Alpine from 'alpinejs';
+import "./bootstrap";
+import "flowbite";
+import Alpine from "alpinejs";
+import Chart from "chart.js/auto";
 
 window.Alpine = Alpine;
 Alpine.start();
