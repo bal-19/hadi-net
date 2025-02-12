e625800a0dba6d78695398a2694ffa8d2971e336
bal-19 <iqbalhaidee19@gmail.com>
2025-02-12 13:36:08 +0700

    style: change register hover style

--- resources/views/user/layouts/_nav-guest.blade.php
+++ resources/views/user/layouts/_nav-guest.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route('register') }}"
-    class="text-white bg-indigo-700 hover:bg-indigo-800 duration-300 transition-all focus:ring-4 focus:ring-indigo-30 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">Register</a>
+    class="text-white bg-indigo-700 hover:bg-white hover:text-indigo-800 hover:border hover:border-indigo-700 duration-300 transition-all focus:ring-4 focus:ring-indigo-30 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">Register</a>
 <a href="{{ route('login') }}"
     class="text-indigo-700 hover:text-white border border-indigo-700 duration-300 transition-all hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-indigo-500 dark:text-indigo-500 dark:hover:text-white dark:hover:bg-indigo-500 dark:focus:ring-indigo-800">Login</a>
