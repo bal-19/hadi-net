80316d39a9f2a11a5ada3675daa1ddf1ebeaa277
bal-19 <iqbalhaidee19@gmail.com>
2025-02-12 13:36:30 +0700

    feat: import jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import "./bootstrap";
 import "./loading";
 import "./swal-confirm";
 import "flowbite";
+import $ from "jquery";
 import Alpine from "alpinejs";
 import Chart from "chart.js/auto";
 import Swal from "sweetalert2";
@@ -10,6 +11,7 @@ import Aos from "aos";
 window.Alpine = Alpine;
 window.Chart = Chart;
 window.Swal = Swal;
+window.$ = $;
 
 Alpine.start();
 Aos.init();
